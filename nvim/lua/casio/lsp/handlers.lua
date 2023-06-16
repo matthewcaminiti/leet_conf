b28c335b3b7bdc6dbcd9a510c3f8132998dd0d31
mcaminiti <mcaminiti@helcim.com>
2023-06-16 17:16:39 -0600

    added proper server capability check for onhover highlight

--- nvim/lua/casio/lsp/handlers.lua
+++ nvim/lua/casio/lsp/handlers.lua
@@ -48,7 +48,7 @@ end
 -- Does highlighting on hover
 local function lsp_highlight_document(client)
     -- Set autocommands conditional on server_capabilities
-    if client.server_capabilities.document_highlight then
+    if client.server_capabilities.documentHighlightProvider then
         vim.api.nvim_exec(
         [[
         augroup lsp_document_highlight
